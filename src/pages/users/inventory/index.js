87d7c856fec93e317fdb83a23c8af923c2670d42
Apocryphon@06 <hrithikchandrashekar@gmail.com>
2024-07-27 08:34:48 +0530

    clean up

--- src/pages/users/inventory/index.js
+++ src/pages/users/inventory/index.js
@@ -1,18 +1,28 @@
-import Breadcrumb from '@/components/Breadcrumb'
-import Layout from '@/components/Layout'
-import Head from 'next/head'
-import React from 'react'
+import Breadcrumb from "@/components/Breadcrumb";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React from "react";
 
 let links = [
   {
-    name: 'Home',
-    link: '/users/dashboard',
+    name: "Home",
+    link: "/users/dashboard",
   },
   {
-    name: 'Inventory',
-    link: '#',
+    name: "Outlet Type",
+    link: "#",
   },
-]
+];
+
+let dummy_data = [
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+];
 
 const Inventory = () => {
   return (
@@ -22,18 +32,21 @@ const Inventory = () => {
       </Head>
       <div className="w-full h-full flex flex-col flex-shrink-0">
         <Breadcrumb links={links} />
+        <div className="flex flex-col p-5 w-full">
+          <div className="flex flex-col w-full h-20 bg-white"></div>
+        </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Inventory
+export default Inventory;
 
 export async function getServerSideProps() {
-  let data = {}
+  let data = {};
   return {
     props: {
       data: data,
     },
-  }
+  };
 }
