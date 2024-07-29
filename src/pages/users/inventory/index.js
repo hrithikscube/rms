2bb374e7169a52b038c2ef5ccce8ee281d801e58
hrithikscube <hrithik@scube.me>
2024-07-29 11:29:15 +0530

    inventory

--- src/pages/users/inventory/index.js
+++ src/pages/users/inventory/index.js
@@ -22,6 +22,49 @@ let dummy_data = [
     city: "Bengaluru",
     outlet_type: "out_type_1",
   },
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+  {
+    kitchen_name: "CHAI FAST [id:1234]",
+    type: "type1",
+    state: "Karnataka",
+    city: "Bengaluru",
+    outlet_type: "out_type_1",
+  },
+
 ];
 
 const Inventory = () => {
@@ -33,7 +76,49 @@ const Inventory = () => {
       <div className="w-full h-full flex flex-col flex-shrink-0">
         <Breadcrumb links={links} />
         <div className="flex flex-col p-5 w-full">
-          <div className="flex flex-col w-full h-20 bg-white"></div>
+          <div className="flex flex-col w-full h-20 bg-white items-end justify-center px-4">
+
+            <button className="p-2 text-xs px-7 rounded-lg border border-[#C0C0C0] text-[#121212] font-medium">Save</button>
+
+          </div>
+          <div className="flex flex-col h-12 w-full px-5">
+
+            <div className="grid grid-cols-5 gap-5 items-center h-full">
+
+              {
+                [
+                  "Restaurant/Kitchen",
+                  "Type",
+                  "State",
+                  "City",
+                  "Outlet Type"
+                ].map((item) => (
+                  <p className="text-xs text-[#121212] font-medium">{item}</p>
+                ))
+              }
+
+            </div>
+
+          </div>
+
+          <div className="flex flex-col bg-white">
+            {
+              dummy_data.map(item => (
+                <div className="grid grid-cols-5 gap-5 items-center h-full last:border-none border-b border-[#C0C0C0] p-4">
+                  <p className="text-xs text-[#121212]">{item?.kitchen_name}</p>
+                  <p className="text-xs text-[#121212]">{item?.type}</p>
+                  <p className="text-xs text-[#121212]">{item?.state}</p>
+                  <p className="text-xs text-[#121212]">{item?.city}</p>
+                  <p className="text-xs text-[#121212]">{item?.outlet_type}</p>
+                </div>
+              ))
+            }
+          </div>
+          <div className="flex flex-col w-full h-20 bg-red-50 items-end justify-center px-4">
+
+            <button className="p-2 text-xs px-7 rounded-lg  text-white bg-red-600 font-medium">Save</button>
+
+          </div>
         </div>
       </div>
     </Layout>
