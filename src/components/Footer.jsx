6b875ff45c25cf35418478478eba27f080166c18
hrithikscube <hrithik@scube.me>
2024-08-06 15:07:05 +0530

    updated package.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='h-14 flex w-full items-center px-5 justify-between bg-white flex-shrink-0'>
 
-            <p className='text-sm text-[#121212]'>hms.scube.me &copy; 2024. All Rights Reserved </p>
+            <p className='text-sm text-[#121212]'>rms.scube.me &copy; 2024. All Rights Reserved </p>
 
             <div className='flex items-center gap-5'>
                 <p className='text-sm text-[#121212]'>Terms & Conditions</p>
