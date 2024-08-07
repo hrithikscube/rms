7daaa367f2b444bc64479119c8d7e09c63fcac07
hrithikscube <hrithik@scube.me>
2024-08-07 17:21:52 +0530

    speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { Open_Sans } from "next/font/google";
 const osFont = Open_Sans({ subsets: ["latin"] });
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
