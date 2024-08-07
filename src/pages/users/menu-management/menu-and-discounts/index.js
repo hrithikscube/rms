15c720dc4a319da4fc917f6af6bb0026905776b8
hrithikscube <hrithik@scube.me>
2024-08-07 16:23:05 +0530

    menu management

--- src/pages/users/menu-management/menu-and-discounts/index.js
+++ src/pages/users/menu-management/menu-and-discounts/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Layout from '@/components/Layout';
 import Breadcrumb from '@/components/Breadcrumb';
@@ -18,7 +18,42 @@ let links = [
     },
 ]
 
+let list_of_headers = [
+    {
+        name: 'Items',
+        value: 'Items'
+    },
+    {
+        name: 'Categories',
+        value: 'Categories'
+    },
+    {
+        name: 'Variants',
+        value: 'Variants'
+    },
+    {
+        name: 'Addons',
+        value: 'Addons'
+    },
+    {
+        name: 'Tables/Areas',
+        value: 'Tables/Areas'
+    },
+    {
+        name: 'Taxes',
+        value: 'Taxes'
+    },
+    {
+        name: 'Discounts',
+        value: 'Discounts'
+    },
+
+]
+
 const MenuAndDiscounts = () => {
+
+    const [activeMenu, setActiveMenu] = useState('Categories')
+
     return (
         <Layout>
             <Head>
@@ -26,6 +61,19 @@ const MenuAndDiscounts = () => {
             </Head>
             <div className="w-full h-full flex flex-col flex-shrink-0">
                 <Breadcrumb links={links} />
+                <div className='h-10 flex items-center px-10 justify-evenly w-full bg-blue-50 border-y border-[#808080]/10'>
+
+                    {
+                        React.Children.toArray(list_of_headers.map(item => (
+                            <button
+                                onClick={() => {
+                                    setActiveMenu(item.value)
+                                }}
+                                className={`text-xs font-semibold text-[#121212] h-full px-12 border-2 rounded-lg ${activeMenu === item.value ? 'bg-white border-blue-500' : 'border-transparent'}`}>{item.name}</button>
+                        )))
+                    }
+
+                </div>
             </div>
         </Layout>
     )
