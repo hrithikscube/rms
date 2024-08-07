c926cc36ecd491a4e3e08fb04b2c753b004aa624
hrithikscube <hrithik@scube.me>
2024-08-07 17:45:32 +0530

    insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App({ Component, pageProps }) {
   return (
     <div className={`${osFont.className}`}>
+      <SpeedInsights />
       <Component {...pageProps} />
     </div>
   );
